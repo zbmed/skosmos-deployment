6f8310135237ebdbcf12d715e20daf4fede54ca6
Vincent Kneip <kneip@zbmed.de>
2025-04-22 15:17:18 +0200

    Added branch selection

--- compare_commit_hashes.py
+++ compare_commit_hashes.py
@@ -5,16 +5,17 @@ import shutil
 
 parser = argparse.ArgumentParser(
     formatter_class=RawTextHelpFormatter,
-    description="BEWARE: Running this script will delete a folder named 'log', should it exist.\n\nGiven a repository URL and a reference commit hash, this script fetches the latest commit hash of the given repository's head branch. Afterwards, it outputs y if the commit hashes match, and n otherwise."
+    description="BEWARE: Running this script will delete a folder named 'log', should it exist.\n\nGiven a repository URL, a branch name and a reference commit hash, this script fetches the latest commit hash of the given repository's on the given branch. Afterwards, it outputs y if the commit hashes match, and n otherwise."
 )
 parser.add_argument("--repo_url", help="The URL of the git repository of interest")
 parser.add_argument("--reference_commit", help="The reference commit hash to compare the latest commit hast of the given repository against")
+parser.add_argument("--branch", help="The branch to fetch the latest commit hash from", default=None)
 
 args = parser.parse_args()
 
 # clone the repository
 shutil.rmtree('./log', ignore_errors=True)
-repo = git.Repo.clone_from(args.repo_url, './log', depth=1)
+repo = git.Repo.clone_from(args.repo_url, './log', depth=1, branch=args.branch)
 
 # get the latest commit
 latest_commit = str(repo.head.commit)
